b4765d8d1a92b06dbe22044bceacd8df502ee24a
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-01-10 15:21:45 +0530

    Update ProductExceptSelf.py

--- Day 8/ProductExceptSelf.py
+++ Day 8/ProductExceptSelf.py
@@ -2,36 +2,52 @@ import math
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out=[]
-        left=[]
-        right=[]
-        # 1 1 2  6
-        # 24 12 4 1
-        for i in range(0,len(nums)):
-            if i ==0:
-                left.append(1)
-            else:
-                val =left[i-1]*nums[i-1]
-                left.append(val)
+#  modular approach
+        res=[]
+        prod=1
+
+        for i in nums:
+            res.append(prod)
+            prod*=i
+        prod=1
+
+        for j in range(len(nums)-1,-1,-1):
+            res[j]=res[j]*prod
+            prod*=nums[j]
+
+        return res
+ 
+#         traditonal approach
+#         out=[]
+#         left=[]
+#         right=[]
+#         # 1 1 2  6
+#         # 24 12 4 1
+#         for i in range(0,len(nums)):
+#             if i ==0:
+#                 left.append(1)
+#             else:
+#                 val =left[i-1]*nums[i-1]
+#                 left.append(val)
 
 
                 
-        for j in range(len(nums)-1,-1,-1):
-            if j ==len(nums)-1:
-                right.append(1)
-            else:
-                val=right[-1]*nums[j+1]
-                right.append(val)
-                # val =right[j-1]*nums[j-1]
-                # left.append(val)
+#         for j in range(len(nums)-1,-1,-1):
+#             if j ==len(nums)-1:
+#                 right.append(1)
+#             else:
+#                 val=right[-1]*nums[j+1]
+#                 right.append(val)
+#                 # val =right[j-1]*nums[j-1]
+#                 # left.append(val)
 
-        right=right[::-1]
+#         right=right[::-1]
 
-        for i in range(0,len(left)):
-            val=left[i]*right[i]
-            out.append(val)
+#         for i in range(0,len(left)):
+#             val=left[i]*right[i]
+#             out.append(val)
  
-        return out
+#         return out
 
 
 
