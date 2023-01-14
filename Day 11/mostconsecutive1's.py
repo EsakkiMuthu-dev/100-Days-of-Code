37f88a3732a7ff09aa89e8f4b7d10a7f2c7f1ddd
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-01-14 20:07:52 +0530

    Create mostconsecutive1's.py

---
+++ Day 11/mostconsecutive1's.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        res=0
+        i=0
+        j=1
+        count= 0 if nums[i] == 0 else 1
+
+
+        while j < len(nums) and i < len(nums):
+            if nums[i]!=1:
+                i+=1
+
+            elif nums[j]!=1:
+                res=max(res,count)
+    
+                count=0
+                i=j
+                j+=1 
+      
+            else:
+                count+=1
+                j+=1
+        res=max(res,count)
+        return res
+
+
