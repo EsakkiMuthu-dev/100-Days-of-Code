4fff934f672382edfe8ae6bed5029840371f9f24
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-01-20 20:59:46 +0530

    Update pattern6.py

--- Day 16/pattern6.py
+++ Day 16/pattern6.py
@@ -1,13 +1,23 @@
+#User function Template for python3
+import string
 class Solution:
-    def printDiamond(self, N):
+    def printTriangle(self, N):
         # Code here
-        spaces=N-1
-
+        letters= list(string.ascii_uppercase)
         
-        for i in range(1,N+1):
-            print(" "*spaces + "* "* i)
-            spaces-=1
-        spaces=0        
-        for j in range(N,0,-1):
-            print(" "*spaces + "* "* j)
-            spaces+=1
+        for i in range(N,0,-1):
+            for j in range(i):
+                print(letters[j],end="")
+            print()
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input().strip())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
