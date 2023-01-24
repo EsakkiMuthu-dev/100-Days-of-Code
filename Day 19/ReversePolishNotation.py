59c3b7971d92c8479a84cbccc0259f0e179890ba
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-01-24 18:33:41 +0530

    Update ReversePolishNotation.py

--- Day 19/ReversePolishNotation.py
+++ Day 19/ReversePolishNotation.py
@@ -1,17 +1,31 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack=[]
+        operations={
+            "+":lambda x,y:x+y,
+            "-":lambda x,y:x-y,
+            "*":lambda x,y:x*y,
+            "/":lambda x,y:int(x/y)
+        }
         for c in tokens:
-            if c=="+":
-                stack.append(stack.pop()+stack.pop())
-            elif c=="-":
-                a,b=stack.pop(),stack.pop()
-                stack.append(b-a)
-            elif c=="*":
-                stack.append(stack.pop()* stack.pop())
-            elif c=="/":
-                a,b = stack.pop(),stack.pop()
-                stack.append(int(b/a))
-            else:
+            if c not in operations:
                 stack.append(int(c))
+            else:
+                right=stack.pop()
+                left=stack.pop()
+                stack.append(operations[c](left,right))
         return stack[-1]
+        # for c in tokens:
+        #     if c=="+":
+        #         stack.append(stack.pop()+stack.pop())
+        #     elif c=="-":
+        #         a,b=stack.pop(),stack.pop()
+        #         stack.append(b-a)
+        #     elif c=="*":
+        #         stack.append(stack.pop()* stack.pop())
+        #     elif c=="/":
+        #         a,b = stack.pop(),stack.pop()
+        #         stack.append(int(b/a))
+        #     else:
+        #         stack.append(int(c))
+        # return stack[-1]
