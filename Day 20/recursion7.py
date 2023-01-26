70f784320384161e08febd7444787076995ed465
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-01-26 16:05:32 +0530

    Create recursion7.py

---
+++ Day 20/recursion7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        n1=0
+        n2=1
+
+        for i in range(2,n+1):
+            temp=n1+n2
+            n1,n2=n2,temp
+        return n2
+
+        # if n==0:
+        #     return 0
+        # if n ==1:
+        #     return 1
+        # return self.fib(n-1)+ self.fib(n-2)
