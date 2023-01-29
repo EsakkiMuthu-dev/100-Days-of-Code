54f4e9d6767e6984f25a852ec0ac63fe3803ec0a
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-01-29 21:36:31 +0530

    Create MaxNumber.py

---
+++ Day 21/MaxNumber.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+def largest( arr, n):
+    max=float('-inf')
+    
+    for i in arr:
+        if i > max:
+            max=i
+    return max
+    
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(largest(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
