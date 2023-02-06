a74acb4ed80a77cc9644c05acf2e954684cfa249
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-02-06 21:20:40 +0530

    Create SearchInMatrix.py

---
+++ Day 27/SearchInMatrix.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+class Solution:
+	def matSearch(self,mat, N, M, X):
+		# Complete this function
+		 for i in range(N):
+		     for j in range(M):
+		         if mat[i][j] == X:
+		             return 1
+		 return 0
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n, m = map(int, input().split())
+        
+        arr = [int(x) for x in input().split()]
+        x = int(input())
+        mat = [[0 for j in range(m)] for i in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                mat[i][j] = arr[i * m + j]
+        ob = Solution()
+        print(ob.matSearch(mat, n, m, x))
+# } Driver Code Ends
