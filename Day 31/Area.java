b8898bbecec7745d087ec4e190796cffb94997b4
EsakkiMuthu <esakkimuthu6842@gmail.com>
2023-02-11 15:23:39 +0530

    Create Area.java

---
+++ Day 31/Area.java
@@ -0,0 +1,17 @@
+public class Area
+{
+    public static void main(String[] args) {
+        // Area of circle
+        int radius = 6;
+        float pi = Math.round(Math.PI);
+        float area = pi * radius*radius;
+        System.out.println(area);
+
+        //area of triangle
+        int base =7,height=8;
+        float areaTri = (base*height)/2;
+        System.out.println(areaTri);
+
+        // 
+    }
+}
