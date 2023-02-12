7e75a6ff987d172fe05996b87194fb021e47e1c0
EsakkiMuthu <esakkimuthu6842@gmail.com>
2023-02-12 05:59:05 +0530

    Create ArrayandLst.java

---
+++ Day 32/ArrayandLst.java
@@ -0,0 +1,12 @@
+
+public class ArrayandLst {
+    public static void main(String[] args) {
+        //  another
+        int[] arr1 ={1,2,3,4,5};
+        int[] arr = new int[5];
+        System.out.println(arr[0]);
+        
+        System.out.println(arr1[0]);
+        
+    }
+}
