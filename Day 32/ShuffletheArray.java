314c1808576def2e1d4422fcd4381bebe0c882eb
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-02-12 10:35:26 +0530

    Create ShuffletheArray.java

---
+++ Day 32/ShuffletheArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2*n];
+
+        for(int i =0;i<n;i++)
+        {
+            res[2*i] =nums[i];
+            res[2*i+1] = nums[i+n];
+
+        }
+        return res;
+    }
+}
