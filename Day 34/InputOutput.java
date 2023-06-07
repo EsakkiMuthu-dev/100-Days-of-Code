216195c1ce94fd376f75e59f672ef19972ce4cbc
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-06-07 14:38:53 +0530

    Create InputOutput.java

---
+++ Day 34/InputOutput.java
@@ -0,0 +1,10 @@
+//User function Template for Java
+class Solution{
+    static void printIntString(String S,int N){
+        // code here
+        System.out.println("The input string :"+S);
+        System.out.println("The input integer :"+N);
+        
+    }
+    
+}
