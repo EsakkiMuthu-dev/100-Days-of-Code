5f109c62afd60a6d13cd851f7ee90c8451bbf287
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-06-10 15:25:09 +0530

    Create IsEligible.java

---
+++ Day 35/IsEligible.java
@@ -0,0 +1,9 @@
+    public static void IsEligible(int age)
+    {
+        if(age>= 18)
+        {
+            System.out.println("He/She is Eligible");
+        }else{
+            System.out.println("He/She is not Eligible");
+        }
+    }
