9acf00f8c4a27dbfdcc216ea27d941973330873e
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-10-10 21:59:31 +0530

    Create BMICalculator.js

---
+++ Day 44/BMICalculator.js
@@ -0,0 +1,20 @@
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
+function bmiCalculator(weight,height)
+{
+   let bmi = weight/height
+    return Math.round(bmi);
+}
+var bmi = bmiCalculator(65, 1.8); 
+console.log(bmi);
