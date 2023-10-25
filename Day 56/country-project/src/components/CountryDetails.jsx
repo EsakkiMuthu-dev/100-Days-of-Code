4fea65794444deb851b5b825caddaf1a34b93de9
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-10-25 10:40:18 +0530

    Update CountryDetails.jsx

--- Day 56/country-project/src/components/CountryDetails.jsx
+++ Day 56/country-project/src/components/CountryDetails.jsx
@@ -12,18 +12,22 @@ export default function CountryDetails({country}) {
     }
     
   return (
-    <div>
-        <h1>{country.name.common}</h1>
-        <p>Capital: {country.capital[0]}</p>
-        <p>Area: {country.area}</p>
+    <>
+    {country && ( 
+        <div>
+            <h1>{country.name.common}</h1>
+            <p>Capital: {country.capital[0]}</p>
+            <p>Area: {country.area}</p>
 
-        <h3>Languages</h3>
-            <ul>
-                {fetchLang().map((val,index)=> <li key={index}>{val}</li>) }
-            </ul>
-        
-        <img src={country.flags.png} alt={country.flags.alt} />
-    </div>
+            <h3>Languages</h3>
+                <ul>
+                    {fetchLang().map((val,index)=> <li key={index}>{val}</li>) }
+                </ul>
+            
+            <img src={country.flags.png} alt={country.flags.alt} />
+        </div>
+       )}
+    </>
 
   )
 }
