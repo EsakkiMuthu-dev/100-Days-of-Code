5c4a8497cbfa7ede3e061c6c91924efc940c1c11
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-11-03 22:09:10 +0530

    WIP

--- Day 61/MyFriend/src/components/WebRTCComponent.jsx
+++ Day 61/MyFriend/src/components/WebRTCComponent.jsx
@@ -1,7 +1,43 @@
 import React, { useState, useEffect } from "react";
+import SimplePeer from "simple-peer";
 
 const WebRTCComponent = () => {
-  // Your WebRTC logic goes here
+  const [signalData, setSignalData] = useState(null);
+  const [peer, setPeer] = useState(null);
+
+  useEffect(() => {
+    const initializePeer = () => {
+      const newPeer = new SimplePeer({ initiator: true });
+
+      newPeer.on("signal", (data) => {
+        console.log("Signal data:", data);
+        setSignalData(data);
+        // Send this signal data to the other peer
+      });
+
+      newPeer.on("connect", () => {
+        console.log("Peer connected");
+        // Handle connection establishment
+      });
+
+      newPeer.on("data", (data) => {
+        console.log("Received data:", data);
+        // Handle the received data
+      });
+
+      setPeer(newPeer);
+    };
+
+    initializePeer();
+
+    // Cleanup function
+    return () => {
+      if (peer) {
+        peer.destroy();
+      }
+    };
+  }, []);
+
   return <div>WebRTC Component</div>;
 };
 
