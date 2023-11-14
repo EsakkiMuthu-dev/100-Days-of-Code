9b127c3be9a5f16348831782f0dc13f8c01b1690
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-11-14 09:26:39 +0530

    password generator done!

--- Day 69/PasswordGenerator/src/App.jsx
+++ Day 69/PasswordGenerator/src/App.jsx
@@ -1,21 +1,69 @@
-import { useState } from 'react'
+import { useEffect, useState ,useCallback} from 'react'
 import { Input,InputGroup,InputRightElement,Button } from '@chakra-ui/react'
 import {
   Slider,
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
-  SliderMark,
+  SliderMark,Container
 } from '@chakra-ui/react'
 import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+
 function App() {
   const[password,setPassword]=useState('');
   const [length, setLength] = useState(8);
+  const[copied,setCopied]=useState(false);
   const[numbersAllowed,setNumbersAllowed]=useState(false);
   const[charactersAllowed,setCharactersAllowed]=useState(false);
 
+
+  const generatePassword= useCallback(()=>{
+      let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let password = "";
+    
+      if (numbersAllowed) {
+        charset += "0123456789";
+      }
+    
+      if (charactersAllowed) {
+        charset += "!@#$%^&*()_+[]{}|;:,.<>?";
+      }
+    
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+      }
+    
+       setPassword(password);
+    
+    },[length,numbersAllowed,charactersAllowed])
+
+  
+ 
+  useEffect(()=>{
+    // Debounce user input
+    const timeoutId = setTimeout(() => {
+      generatePassword();
+    }, 500);
+
+  },[length,numbersAllowed,charactersAllowed])
+
+
+  const handleCopy= async ()=>{
+    try{
+      await navigator.clipboard.writeText(password);
+      setCopied(true);
+      setTimeout(()=>{
+        setCopied(false)
+      },2500)
+    }catch(e){
+      console.log(`unable to copy to clipboard ${e}`)
+    }
+  }
+
   return (
     <>
+    <Container maxW='md' bg='blue.600' centerContent color='white'>
      <h1>Password Generator</h1>
      <InputGroup size='md'>
       <Input
@@ -26,8 +74,10 @@ function App() {
         value={password}
       />
       <InputRightElement width='4.5rem'>
-        <Button h='1.75rem' size='sm' >
-          copy
+        <Button h='1.75rem' size='sm' 
+        colorScheme={copied?"green":"gray"}
+        onClick={handleCopy} >
+          {copied?"copied!":"copy"}
         </Button>
       </InputRightElement>
     </InputGroup>
@@ -42,6 +92,7 @@ function App() {
 
 <Checkbox isChecked={numbersAllowed} onChange={()=>setNumbersAllowed((prev)=>!prev)}>Numbers</Checkbox>
 <Checkbox isChecked={charactersAllowed}onChange={()=>setCharactersAllowed((prev)=>!prev)}>Characters</Checkbox>
+</Container>
     </>
   )
 }
