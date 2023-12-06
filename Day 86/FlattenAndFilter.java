7920096533fffb57d07e6cf10caf37e96bb8a2ed
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-12-06 09:18:39 +0530

    first problem flatten and filter

---
+++ Day 86/FlattenAndFilter.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class FlattenAndFilter {
+    public static void main(String[] args) {
+        List<Integer> ls1 = Arrays.asList(1,2,3,4);
+        List<Integer> ls2 = Arrays.asList(11,22,33,14);
+        List<Integer> ls3 = Arrays.asList(6,3,7,8);
+        List<List<Integer>> totalList = Arrays.asList(ls1,ls2,ls3);
+        flattenAndFilter(totalList);
+    }
+    public static List<Integer> flattenAndFilter(List<List<Integer>> nestedLists) {
+        List<Integer> result = nestedLists.stream()
+                                           .flatMap(ls->ls.stream())
+                                           .collect(Collectors.toList());
+
+        return result;
+
+    }
+}
