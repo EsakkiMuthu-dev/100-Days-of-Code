3cf7d9ea0759e0187b9d182be6d3b000a9ea4f65
Esakki Muthu <esakkimuthu6842@gmail.com>
2023-12-25 09:37:32 +0530

    ant learning

--- Day 97/palindrome.js
+++ Day 97/palindrome.js
@@ -2,6 +2,7 @@
 Write a function called isPalindrome that takes a string as an argument and returns true if the string is a palindrome (reads the same backward as forward), and false otherwise. */
 function reverse(str)
 {
+    
     return str.reverse();
 }
 
