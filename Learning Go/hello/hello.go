91ac20673507cbc5824f27a59be36bfc11bd924a
EsakkiMuthu <esakkimuthu6842@gmail.com>
2026-05-03 12:13:03 +0530

    First step

---
+++ Learning Go/hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(" Hey I am From hello module...!!!")
+	fmt.Println("<------------------->")
+	fmt.Println(quote.Glass())
+	fmt.Println("<------------------->")
+}
